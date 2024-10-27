31773537573ab2bf63cc982fd416fea84526edd5
Babak Jameei <babak.jameei@outlook.com>
2024-10-27 14:38:33 +0100

    app routes uppdaterade

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: TodoListComponent
+    },
+    // ... andra routes ...
+    { 
+        path: '**', 
+        redirectTo: '' 
+    }
+];
